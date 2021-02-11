0dac366b3f933f492db2587930e3170c7a97a66d
Mile Jankuloski <mile.jankuloski@students.finki.ukim.mk>
2021-02-11 18:05:54 +0100

    Initial project

---
+++ src/main/java/mk/vezbanka/wp/VezbankaApplication.java
@@ -0,0 +1,13 @@
+package mk.vezbanka.wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VezbankaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VezbankaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/vezbanka/wp/VezbankaApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.vezbanka.wp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VezbankaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
